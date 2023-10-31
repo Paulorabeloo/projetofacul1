6dada8c13a55f12bfd1e575d2eaf61229fcf98b0
Paulorabeloo <pauloeduardorabelomiranda@gmail.com>
2023-10-31 16:26:33 -0300

    projetofacul1

---
+++ projetofacul1.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+int main () {
+
+    int numero1;
+    int numero2;
+    int adicao;
+    int subtracao;
+    int multiplicacao;
+    float divisao;
+
+    numero1 = 10;
+    numero2 = 20;
+    adicao = numero1 + numero2;
+    subtracao = numero1 - numero2;
+    multiplicacao = numero1 * numero2;
+    divisao = numero1 / numero2;
+
+    printf("%i\n", adicao);
+    printf("%i\n", subtracao);
+    printf("%i\n", multiplicacao);
+    printf("%f\n", divisao);
+
+
+return 0;
+}
